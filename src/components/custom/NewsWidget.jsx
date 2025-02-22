2b6123ca09c59cdc341ff2256b85596d13008b60
İlyas Torun <ilyastorunn@outlook.com>
2025-02-22 11:31:19 +0300

    minor UI updates

--- src/components/custom/NewsWidget.jsx
+++ src/components/custom/NewsWidget.jsx
@@ -1,40 +1,31 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { CiLocationArrow1 } from "react-icons/ci";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function NewsWidget() {
   return (
-    <Card className="w-full h-full min-h-[250px] bg-card p-4 shadow flex flex-col justify-between border-none">
-      <CardHeader className="flex-row items-start -space-y-1 gap-2 p-0 pb-4">
-        <span className="text-lg font-semibold text-text-primary">
+    <div className="w-full">
+        <span className="text-lg font-semibold text-text-primary hover:underline block mb-2">
           Check out news
         </span>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-text-secondary hover:text-text-primary"
-        >
-          <CiLocationArrow1 className="h-4 w-4" />
-        </Button>
-      </CardHeader>
-      <div className="w-full h-[180px] overflow-hidden rounded-lg">
-        <img
-          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
-          alt="News"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <CardContent className="pb-4 pl-0 mt-auto">
-        <div className="mb-2 text-sm text-text-secondary">
-          What&apos;s happening today
+      <Card className="w-full h-full min-h-[250px] bg-card p-4 shadow flex flex-col justify-between border-none">
+        <div className="w-full h-[180px] overflow-hidden rounded-lg">
+          <img
+            src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
+            alt="News"
+            className="w-full h-full object-cover pb-4"
+          />
         </div>
-        <div className="text-sm leading-relaxed text-text-primary">
-          US jobless claims hit 5-week low at 211,000. Treasury yields were
-          stable post weak inflation data reports, and UBS faces Swiss law
-          challenges.
-        </div>
-      </CardContent>
-    </Card>
+        <CardContent className="pb-4 pl-0 mt-auto">
+          <div className="mb-2 text-sm text-text-secondary">
+            What&apos;s happening today
+          </div>
+          <div className="text-sm leading-relaxed text-text-primary">
+            US jobless claims hit 5-week low at 211,000. Treasury yields were
+            stable post weak inflation data reports, and UBS faces Swiss law
+            challenges.
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
